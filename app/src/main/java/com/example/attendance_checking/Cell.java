3ec5e4208fb6dbc1630d6a17690fbc2f1b903ce1
rusya-ink <43722585+rusya-ink@users.noreply.github.com>
2020-03-16 18:56:54 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/attendance_checking/Cell.java
@@ -0,0 +1,10 @@
+package com.example.attendance_checking;
+
+public class Cell {
+    int id;
+    boolean type;
+    public Cell(int id, boolean type){
+        this.id = id;
+        this.type = type;
+    }
+}
