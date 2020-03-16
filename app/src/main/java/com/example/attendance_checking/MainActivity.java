88ceaced6754e511b4e92889de0f0317545225d4
rusya-ink <parasyte_pravik@mail.ru>
2020-03-16 21:19:24 +0300

    test

--- app/src/main/java/com/example/attendance_checking/MainActivity.java
+++ app/src/main/java/com/example/attendance_checking/MainActivity.java
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 package com.example.attendance_checking;
+=======
+package com.example.myapplication;
+>>>>>>> d80f29555452d6456babe22e5c63e12e2e862d44
 
 import androidx.appcompat.app.AppCompatActivity;
 
