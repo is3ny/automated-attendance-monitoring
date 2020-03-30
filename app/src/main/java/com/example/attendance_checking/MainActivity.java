8bf132eef0d85fab2c2c6035b012b05f5bcee558
rusya-ink <parasyte_pravik@mail.ru>
2020-03-30 22:09:50 +0300

    Basic Course GUI

--- app/src/main/java/com/example/attendance_checking/MainActivity.java
+++ app/src/main/java/com/example/attendance_checking/MainActivity.java
@@ -2,75 +2,38 @@ package com.example.attendance_checking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout layout;
-    LinkedList<Cell> cells;
-    LinearLayout.LayoutParams params;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cells = new LinkedList<Cell>();
-        params = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
-        layout = ((LinearLayout)findViewById(R.id.LinearLayout_0));
-        ((Button)findViewById(R.id.Remove)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                remove_cell();
-            }
-        });
-        ((Button)findViewById(R.id.Add_plus)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                add_plus_cell();
-            }
-        });
-        ((Button)findViewById(R.id.Add_minus)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                add_minus_cell();
-            }
-        });
+        ListView listView = findViewById(R.id.list_view);
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student());
+        Professor p1 = new Professor("Luiz Jonata");
+        Professor p2 = new Professor("Some Strange Name");
+        Attendance a1 = new Attendance(new Boolean[]{true, false, true});
+        Attendance a2 = new Attendance(new Boolean[]{false, false, true, true, true});
+        Course c1 = new Course("Course number One", p1);
+        Course c2 = new Course("Course number Two", p2);
+        c1.addAttendance(students.get(0), a1);
+        c2.addAttendance(students.get(0), a2);
+        ArrayList<Course> courses = new ArrayList<>();
+        courses.add(c1);
+        courses.add(c2);
+
+
+        CourseListAdapter adapter = new CourseListAdapter(courses, students.get(0), this);
+
+        // настраиваем список
+
+        listView.setAdapter(adapter);
     }
-    protected void remove_cell(){
-        if(cells.size()>0){
-            layout.removeView((FrameLayout)findViewById(cells.getLast().id));
-            cells.removeLast();
-
-        }
-    }
-
-    protected void add_plus_cell(){
-        int size = cells.size();
-        cells.add(new Cell(size, true));
-        FrameLayout new_frame = new FrameLayout(this);
-        new_frame.setLayoutParams(params);
-        new_frame.setId(cells.getLast().id);
-        new_frame.setForeground(new ColorDrawable(Color.GREEN));
-        layout.addView(new_frame);
-    }
-
-    protected void add_minus_cell(){
-        int size = cells.size();
-        cells.add(new Cell(size, false));
-        FrameLayout new_frame = new FrameLayout(this);
-        new_frame.setLayoutParams(params);
-        new_frame.setId(cells.getLast().id);
-        new_frame.setForeground(new ColorDrawable(Color.RED));
-        layout.addView(new_frame);
-    }
-
-
 }
