3885e04e299309754587270e0e7d36144854dfbd
is3ny <supremeep@yandex.ru>
2020-04-05 18:33:42 +0300

    First Travis CI attempt.

---
+++ .travis/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git fetch --unshallow #required for commit count
+
+./gradlew clean assembleStandardDebug
+
+COMMIT_COUNT=$(git rev-list --count HEAD)
+export ARTIFACT="aam-r${COMMIT_COUNT}.apk"
+
+mv app/build/outputs/apk/debug/app-debug.apk $ARTIFACT
