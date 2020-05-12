f45250ca2418ede537b7c96889caf2ce79fb0069
is3ny <supremeep@yandex.ru>
2020-05-12 14:02:36 +0300

    Student sends a name as a beacon. Teacher tracks the presence of the student. But the data fields are not being correctly received and therefore do not allow to register student as of yet.

--- app/src/main/java/com/example/attendance_checking/StudentActivity.java
+++ app/src/main/java/com/example/attendance_checking/StudentActivity.java
@@ -2,6 +2,7 @@ package com.example.attendance_checking;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,17 +10,28 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class StudentActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText ip_et, port_et, msg_et;
-    Button send_btn;
+    Button start_bcast_btn, stop_bcast_btn;
+
+    Beacon beacon;
+    BeaconParser beaconParser;
+    BeaconTransmitter beaconTransmitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,13 @@ public class StudentActivity extends AppCompatActivity implements View.OnClickLi
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.setDebug(true);
+
+        beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+        beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
         /*
         findViewById(R.id.button_send).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,54 +63,35 @@ public class StudentActivity extends AppCompatActivity implements View.OnClickLi
         port_et = findViewById(R.id.port_et);
         msg_et = findViewById(R.id.msg_et);
 
-        send_btn = findViewById(R.id.start_bcast_btn);
-        send_btn.setOnClickListener(this);
+        start_bcast_btn = findViewById(R.id.start_bcast_btn);
+        stop_bcast_btn = findViewById(R.id.stop_bcast_btn);
+        start_bcast_btn.setOnClickListener(this);
+        stop_bcast_btn.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.start_bcast_btn) {
-            int port = 1;
-            DatagramSocket socket;
-            InetAddress address;
-
-            // Check if correct port number was supplied before we do all this net mumbo-jumbo.
-            try {
-                port = Integer.parseInt(port_et.getText().toString());
-            } catch (Exception e) {
-                Toast.makeText(this, "Invalid port number.", Toast.LENGTH_LONG).show();
-            }
-
-            try {
-                address = InetAddress.getByName(ip_et.getText().toString());
-            } catch (UnknownHostException e) {
-                Toast.makeText(this, "Failed to parse IP address.", Toast.LENGTH_LONG)
-                     .show();
-                e.printStackTrace();
-                return;
-            }
-
-            try {
-                socket = new DatagramSocket();
-            } catch (SocketException e) {
-                Toast.makeText(this, "Failed to init a socket.", Toast.LENGTH_LONG)
-                        .show();
-                e.printStackTrace();
-                return;
-            }
-
-            byte[] buf = msg_et.getText().toString().getBytes();
-            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, port);
-
-            try {
-                socket.send(packet);
-            } catch (IOException e) {
-                Toast.makeText(this, "Failed to send packet.", Toast.LENGTH_LONG).show();
-                e.printStackTrace();
-            }
-
-            socket.close();
+            Log.i("ratata", "Was " + beaconTransmitter.isStarted());
+            String name = msg_et.getText().toString();
+            beacon = new Beacon.Builder()
+                    .setId1("2f234454-cf6d-4a0f-adf2-f4911ba9ffa6")
+                    .setId2("1")
+                    .setId3("2")
+                    .setManufacturer(0x0118)
+                    .setTxPower(-59)
+                    .setDataFields(Arrays.asList(StringToLongArrayPacker.toLongArray(name)))
+                    //.setDataFields(Collections.singletonList((long) msg_et.getText().toString().length()))
+                    .build();
+            beaconTransmitter.stopAdvertising();
+            beaconTransmitter.setBeacon(beacon);
+            beaconTransmitter.startAdvertising();
+            Toast.makeText(this, "Started transmitting..." + beaconTransmitter.isStarted(), Toast.LENGTH_SHORT).show();
+        } else if (view.getId() == R.id.stop_bcast_btn) {
+            beaconTransmitter.stopAdvertising();
+            Toast.makeText(this, "Stopped transmitting...", Toast.LENGTH_SHORT).show();
         }
     }
 }
